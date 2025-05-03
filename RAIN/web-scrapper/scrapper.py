9fce1ce9bbfa2976cf06432a992e997f0eecc670
jozisatler2 <54095559+jozisatler@users.noreply.github.com>
2025-05-03 14:04:33 +0200

    SCRUM-21 Implemented basic web scrapper with geocoding landmarks

---
+++ RAIN/web-scrapper/scrapper.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+import time
+import json
+import subprocess
+import urllib.parse
+
+def geocode(place): # Pretvori ime znamenitosti v GPS koordinate
+    encoded_place = urllib.parse.quote(place)
+    curl = ['curl', f"https://nominatim.openstreetmap.org/search?q={encoded_place}&limit=2&format=json"]
+    result = subprocess.run(curl, capture_output=True, text=True)
+    if result.returncode == 0:
+        data = json.loads(result.stdout)
+        if data:
+            return f"{data[0]['lat']},{data[0]['lon']}"
+    return "-,-"
+
+session = HTMLSession()
+response = session.get("https://www.visitmaribor.si/si/kaj-poceti/znamenitosti/")
+response.html.render() # izvede javascript kodo
+
+soup = BeautifulSoup(response.html.html, "html.parser")
+landmarks = soup.find_all("h3", attrs={"class":"catalogue-item__title"})
+
+for landmark in landmarks:
+    name = landmark.text.strip()
+    print(name)
+    print(geocode(name))
+    time.sleep(1)
