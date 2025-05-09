3d13b4f44b0de37f8a1970f1503ad6fbcfb86f51
GalPovsod8 <galpos10@gmail.com>
2025-05-09 23:01:57 +0200

    SCRUM-56 Added logout functionality

--- NPO/app/components/ui/LoggedInProfile.tsx
+++ NPO/app/components/ui/LoggedInProfile.tsx
@@ -1,4 +1,8 @@
+import { Colors } from "@/constants/Colors";
 import { useUserStore } from "@/store/useUserStore";
+import { Button } from "@react-navigation/elements";
+import { Platform } from "react-native";
+import Toast from "react-native-toast-message";
 import ParallaxScrollView from "../ParallaxScrollView";
 import { ThemedText } from "../ThemedText";
 import { ThemedView } from "../ThemedView";
@@ -6,15 +10,54 @@ import { ThemedView } from "../ThemedView";
 //zdaj si v pravem: SCRUM-56-Uporabniški-profil - TODO
 
 export function LoggedInProfile() {
+  const toastPosition = Platform.OS === "ios" ? "top" : "bottom";
   const { user } = useUserStore();
+
+  let logout = async () => {
+    const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
+    try {
+      const response = await fetch(`${apiUrl}/users/logout`, {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        useUserStore.getState().clearUser();
+        Toast.show({
+          type: "success",
+          text1: "Logged out successfully",
+          position: toastPosition,
+        });
+      } else {
+        const err = await response.json();
+        throw new Error(err.message || "Logout failed");
+      }
+    } catch (err: any) {
+      Toast.show({
+        type: "error",
+        text1: "Logout failed",
+        text2: err.message || "An error occurred",
+        position: toastPosition,
+      });
+    }
+  };
 
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "transparent", dark: "transparent" }}
     >
       <ThemedText type="title">{`Hello, ${user?.username}`}</ThemedText>
-      <ThemedView>
+      <ThemedView style={{ gap: 16 }}>
         <ThemedText type="subtitle">Stats</ThemedText>
+        <Button
+          variant="filled"
+          color={Colors.primary}
+          style={{ borderRadius: 16, paddingVertical: 16 }}
+          onPress={logout}
+        >
+          Logout
+        </Button>
       </ThemedView>
     </ParallaxScrollView>
   );
