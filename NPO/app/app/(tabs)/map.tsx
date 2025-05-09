521d8c4a9b27286bc2a5475e8b0da8bb33616d50
GalPovsod8 <galpos10@gmail.com>
2025-05-09 13:50:05 +0200

    SCRUM-31 Implemented location showing and detection on map

--- NPO/app/app/(tabs)/map.tsx
+++ NPO/app/app/(tabs)/map.tsx
@@ -1,10 +1,64 @@
-import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Alert, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function MapScreen() {
+  const [location, setLocation] =
+    useState<Location.LocationObjectCoords | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission denied", "Location permission is required.");
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation.coords);
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading || !location) {
+    return (
+      <ThemedView style={styles.loader}>
+        <ActivityIndicator size="large" />
+      </ThemedView>
+    );
+  }
+
   return (
-    <ThemedView>
-      <ThemedText type="title">Map</ThemedText>
-    </ThemedView>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: location.latitude,
+        longitude: location.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    >
+      <Marker
+        coordinate={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+        }}
+        title="Tukaj si"
+      />
+    </MapView>
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
