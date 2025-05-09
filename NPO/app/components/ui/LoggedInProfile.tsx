5d1dffd96f2db88beb29992b1d443f67df922dbe
GalPovsod8 <galpos10@gmail.com>
2025-05-09 22:41:45 +0200

    SCRUM-56 Initial profile design and implementation

--- NPO/app/components/ui/LoggedInProfile.tsx
+++ NPO/app/components/ui/LoggedInProfile.tsx
@@ -1,12 +1,21 @@
+import { useUserStore } from "@/store/useUserStore";
 import ParallaxScrollView from "../ParallaxScrollView";
 import { ThemedText } from "../ThemedText";
+import { ThemedView } from "../ThemedView";
+
+//zdaj si v pravem: SCRUM-56-Uporabniški-profil - TODO
 
 export function LoggedInProfile() {
+  const { user } = useUserStore();
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "transparent", dark: "transparent" }}
     >
-      <ThemedText>Profil</ThemedText>
+      <ThemedText type="title">{`Hello, ${user?.username}`}</ThemedText>
+      <ThemedView>
+        <ThemedText type="subtitle">Stats</ThemedText>
+      </ThemedView>
     </ParallaxScrollView>
   );
 }
