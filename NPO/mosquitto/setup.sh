00fa8605dcf4d9fa21423121633b75b9727680bc
jozisatler2 <54095559+jozisatler@users.noreply.github.com>
2025-05-12 21:44:04 +0200

    Removed sudo commands

--- NPO/mosquitto/setup.sh
+++ NPO/mosquitto/setup.sh
@@ -3,4 +3,4 @@
 mkdir -p ./mosquitto/data
 mkdir -p ./mosquitto/log
 
-sudo docker run -d --name mosquitto-container -p 1883:1883 -v $(pwd)/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $(pwd)/mosquitto/data:/mosquitto/data -v $(pwd)/mosquitto/log:/mosquitto/log eclipse-mosquitto
+docker run -d --name mosquitto-container -p 1883:1883 -v $(pwd)/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $(pwd)/mosquitto/data:/mosquitto/data -v $(pwd)/mosquitto/log:/mosquitto/log eclipse-mosquitto
--- RAIN/mongo/setup.sh
+++ RAIN/mongo/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker network create backend-net
-sudo docker run -d --name mongo-container --network backend-net -p 27017:27017 -v mongo-data:/data/db mongo
+docker network create backend-net
+docker run -d --name mongo-container --network backend-net -p 27017:27017 -v mongo-data:/data/db mongo
