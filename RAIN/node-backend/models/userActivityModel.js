0e8a095ae507db4d78f530a010a1740e7a584439
GalPovsod8 <galpos10@gmail.com>
2025-05-27 17:43:58 +0200

    updated user activity model

--- RAIN/node-backend/models/userActivityModel.js
+++ RAIN/node-backend/models/userActivityModel.js
@@ -1,13 +1,18 @@
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 var userActivitySchema = new Schema({
-	'user' : {
-	 	type: Schema.Types.ObjectId,
-	 	ref: 'user'
-	},
-	'date' : Date,
-	'visited' : Array
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  date: Date,
+  visited: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "landmark",
+    },
+  ],
 });
 
-module.exports = mongoose.model('userActivity', userActivitySchema);
+module.exports = mongoose.model("userActivity", userActivitySchema);
