8e3c38f5563fb14f75249154741234699d5ba98d
jozisatler2 <54095559+jozisatler@users.noreply.github.com>
2025-05-27 17:02:38 +0200

    SCRUM-62 Fixed side setup scripts

--- NPO/mosquitto/setup.sh
+++ NPO/mosquitto/setup.sh
@@ -3,4 +3,4 @@
 mkdir -p ./mosquitto/data
 mkdir -p ./mosquitto/log
 
-docker run -d --name mosquitto-container -p 1883:1883 -v $(pwd)/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $(pwd)/mosquitto/data:/mosquitto/data -v $(pwd)/mosquitto/log:/mosquitto/log eclipse-mosquitto
+docker run -d --name mosquitto-container --network backend-net -p 1883:1883 -v $(pwd)/mosquitto/config/mosquitto.conf:/mosquitto/config/mosquitto.conf -v $(pwd)/mosquitto/data:/mosquitto/data -v $(pwd)/mosquitto/log:/mosquitto/log eclipse-mosquitto
